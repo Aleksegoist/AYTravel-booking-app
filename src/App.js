12fc517ee361594711e651dcfeb86ce7f36ab97e
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-10 10:44:53 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Home from "./pages/home/Home";
-import Hotel from "./pages/hotel/Hotel";
-import List from "./pages/list/List";
-
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Hotel from './pages/hotel/Hotel';
+import List from './pages/list/List';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hotels" element={<List />} />
-        <Route path="/hotels/:id" element={<Hotel />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/hotels' element={<List />} />
+                <Route path='/hotels/:id' element={<Hotel />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
